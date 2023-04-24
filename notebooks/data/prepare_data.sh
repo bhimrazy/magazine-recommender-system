992085b09fe2451da06b9e45306a5c8be9f43b39
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2023-04-24 05:41:37 +0000

    Updated commands

--- notebooks/data/prepare_data.sh
+++ notebooks/data/prepare_data.sh
@@ -6,13 +6,5 @@ wget -c --no-check-certificate https://jmcauley.ucsd.edu/data/amazon_v2/category
 # download products data
 wget -c --no-check-certificate https://jmcauley.ucsd.edu/data/amazon_v2/metaFiles2/meta_Magazine_Subscriptions.json.gz
 
-# unzipping files
-gzip -d Magazine_Subscriptions.json.gz
-gzip -d meta_Magazine_Subscriptions.json.gz
-
-# remove zip files
-rm Magazine_Subscriptions.json.gz
-rm meta_Magazine_Subscriptions.json.gz
-
-mv Magazine_Subscriptions.json notebooks/data/
-mv meta_Magazine_Subscriptions.json notebooks/data/
+mv Magazine_Subscriptions.json.gz notebooks/data/
+mv meta_Magazine_Subscriptions.json.gz notebooks/data/
